82a8f7d72b52297b98e9c3b9cc07bde473b36dbe
ItzRichie <38105787+ItzRichie@users.noreply.github.com>
2022-10-25 09:34:55 -0300

    Update math_suff.cpp

--- math_suff.cpp
+++ math_suff.cpp
@@ -17,6 +17,10 @@ int gcd(int a, int b){
 // NOTE: lcm(a, b) can be implemented as a*b/gcd(a, b). A independent implementation will be provided soon though
 
 int is_prime(long long int n){ // O(N^1/2)
+    
+    if(n==1)
+        return 0;
+    
     for(int i = 2; i<sqrt(n); i++){
         if(n%i==0)
             return 0;
